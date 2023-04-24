33935867cc49a60db43969386c86f6d335442f0e
Nikhil Raj S <nikhil.raj@experionglobal.com>
2023-04-25 00:46:19 +0530

    added description in product page

--- category.php
+++ category.php
@@ -4,19 +4,24 @@
     <div class="app-l-cat__section">
         <div class="container">
             <h2 class="h2 text-white text-center">Explore <?php echo single_cat_title('' , true ); ?></h2>
+						<div class="text-center mb-5 fs-5 fw-light lh-sm" style="color: #868E96;">
+							<?php if(get_field('product_description')) { ?>
+								<?php echo get_field('product_description') ?>
+							<?php } ?>
+						</div>
             <div class="app-l-cat__wrap">
             <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 					$category = get_queried_object();
 					$getCatId = $category->term_id;
 					$args = array('post_type'=> 'websites', 'cat' => $getCatId, 'posts_per_page' => 9, 'paged' => $paged,  );
-                   
+
 					$wp_query = new WP_Query($args);
-                     if (have_posts()) : 
+                     if (have_posts()) :
 					while ( have_posts() ) : the_post(); ?>
                     <div class="app-l-cat__block post-<?php the_ID(); ?>">
                         <div class="app-l-cat__hover">
                             <div class="app-l-cat__feature-img">
-                                <a href="<?php the_permalink() ?>"> 
+                                <a href="<?php the_permalink() ?>">
                                     <?php if ( has_post_thumbnail($post->ID) ) {
                                         $thumb_id = get_post_thumbnail_id();
                                         $thumb_url = wp_get_attachment_image_src($thumb_id,'category', true);
@@ -28,12 +33,12 @@
 
                                     <img data-src="<?php bloginfo('template_directory'); ?>/img/default-image.png" alt="<?php the_title(); ?>" class="">
 
-                                    <?php } ?> 
+                                    <?php } ?>
                                 </a>
                             </div>
                             <div class="app-l-cat__data">
                                 <div class="app-l-cat__tp">
-                                    <?php if( get_field('night') ): ?>   
+                                    <?php if( get_field('night') ): ?>
                                     <span class="app-l-cat__sub-text"><?php echo the_field('tagline') ?></span>
                                     <?php endif; ?>
                                     <h3 class="h5"><a href="<?php the_permalink() ?>"> <?php echo the_title(); ?></a></h3>
@@ -46,16 +51,16 @@
                                                 <div class="app-l-cat__d-num"><span><?php echo the_field('day') ?></span> days</div>
                                             </div>
                                         <?php endif; ?>
-                                        <?php if( get_field('night') ): ?>    
+                                        <?php if( get_field('night') ): ?>
                                             <div class="app-l-cat__dns">
                                                 <i class="folka-night"></i>
                                                 <div class="app-l-cat__d-num"><span><?php echo the_field('night') ?></span> night</div>
                                             </div>
                                         <?php endif; ?>
                                     </div>
-                                    <?php if( get_field('price') ): ?>    
+                                    <?php if( get_field('price') ): ?>
                                         <div class="app-l-cat__price">
-                                            <i class="folka-rupee"></i> 
+                                            <i class="folka-rupee"></i>
                                             <span><?php echo the_field('price') ?></span>
                                         </div>
                                     <?php endif; ?>
@@ -69,7 +74,7 @@
                         <div class="app-l-no-search d-flex flex-column w-100">
                             <div class="app-l-no-search__image">
                                 <img src="<?php echo get_bloginfo('template_directory'); ?>/img/no-search.svg" alt="" class="img-fluid">
-                                
+
                             </div>
                             <div class="app-l-no-search__content">
                                 <h3 class="h2">No data on <?php echo single_cat_title('' , true ); ?></h3>
@@ -78,10 +83,10 @@
                         </div>
                     <?php endif; ?>
             </div>
-          
-            <?php 
+
+            <?php
                 $count = get_category($category->term_id)->category_count;
-                    if (  $count > 6 ) : ?>   
+                    if (  $count > 6 ) : ?>
                 <div class="app-l-cat__more text-center">
                     <button class="app-c-btn app-c-btn--secondary">load more blogs</button>
                 </div>
